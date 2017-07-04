410ca996b07d716e9677cd0bdcbeb0c003556bdf
Alexander Bakst <abakst@cs.ucsd.edu>
2017-07-04 10:41:36 -0700

    cleanup

--- app/Scenario.hs
+++ app/Scenario.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE TemplateHaskell #-}
 module ThequeScenario (main) where
 
+import Data.Char
 import System.IO
 import Control.Distributed.Process.Closure
 import Control.Distributed.Process.Backend.SimpleLocalnet
@@ -16,27 +17,28 @@ import Control.Distributed.Process.SymmetricProcess
 import Control.Distributed.Process.Brisk hiding (call)
 import Control.Distributed.Process.ManagedProcess.Brisk
 
+readTagRefs :: IO [TagRef]
+readTagRefs = undefined
+
+data Cmd = Get | Add | Alloc | Push | CGetBlob
+  deriving (Show, Read)
+
 testClient :: ProcessId -> Process ()
-testClient m
-  = do cmd  <- liftIO $ getChar
-       tag  <- liftIO $ getLine
-       rpc  <- case cmd of
-         'a' -> do ref <- liftIO $ getLine
-                   return (AddTag (TagId tag) [OtherTag (TagId ref)])
-         'g' -> return (GetTag (TagId tag))
-       resp <- call m rpc
-       case resp of
-         M.OK                    -> return ()
-         M.TagRefs rs            -> return ()
-         M.AddBlobServers bid ps -> return ()
+testClient masta
+  = do (cmd, dat, name, refs) <- liftIO $ undefined
+       rpc     <- case read cmd of
+                    Add   -> return $ AddTag (TagId name) refs 
+                    Get   -> return $ GetTag (TagId name) 
+                    Alloc -> return $ AddBlob name 3
+                    Push  -> return $ PushBlob name dat
+                    CGetBlob -> return $ M.GetBlob name
+       call masta rpc :: Process MasterResponse
+       return ()
+
 remotable ['testClient]
 
-spawnMasterClients :: ProcessId -> [NodeId] -> Process (SymSet ProcessId)
-spawnMasterClients m ns
-  = spawnSymmetric ns $ $(mkBriskClosure 'testClient) m
-
-main :: Backend -> [NodeId] -> [NodeId] -> Process ()
-main b slaveNodes clientNodes
-  = do m <- getSelfPid
-       spawnMasterClients m clientNodes
-       runMaster b slaveNodes
+main :: Backend -> NodeId -> [NodeId] -> [NodeId] -> Process ()
+main be mnode slaveNodes clientNodes
+  = do moi  <- getSelfPid
+       spawnSymmetric clientNodes $ $(mkBriskClosure 'testClient) moi
+       runMaster be slaveNodes
